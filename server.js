20a606417a8f9cc1f905443670bd7b9158a2af28
Athena Serene <aserenehoosang@gmail.com>
2022-11-21 11:11:04 -0500

    adding different routes framework

--- server.js
+++ server.js
@@ -14,7 +14,30 @@ app.get("/", (req, res) => res.redirect("/pokedex"))
 app.get("/pokedex", (req, res) => {
     res.render("index.ejs", {pokemon})
 })
-
+app.get("/pokedex/new", (req, res)=> {
+    // res.send("what'cha tryna make?")
+    res.render("new.ejs", {pokemon})
+})
+app.post("/pokedex", (req, res)=> {
+    pokemon.push(req.body)
+    res.redirect("/pokedex")
+})
+app.get("/pokedex/:id", (req, res)=> {
+    // res.send("you're in the right place...")
+    res.render("show.ejs", {
+        poke: pokemon[req.params.id],
+        index: req.params.id
+    })
+})
+app.get("/pokedex/:id/edit", (req, res) => {
+    res.render("edit.ejs")
+})
+app.put("/pokedex/:id", (req, res) => {
+    console.log("this is working")
+})
+app.delete("/pokedex/:id", (req, res) => {
+    res.send("trying to delete?")
+})
 const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`The server is listening on port ${PORT}...`)
